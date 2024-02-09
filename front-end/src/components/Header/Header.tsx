52e2bda1e6951707673943fcf267532f2477ddca
Jowita Gogol <jmg88@bath.ac.uk>
2024-02-09 18:23:58 +0000

    Updated sidebar and header styles

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -5,11 +5,18 @@ const Header = () => {
     <header className="header">
       <div className="header__container">
         <h1 className="header__greeting">Hey there, bookaholic!</h1>
-        <p className="header__explore">Explore the world of (my) books with a click</p>
-      </div>
-      <div className="header__search">
-        <input className="header__search header__search--input" type="text" placeholder="Search..." />
-        <button className="header__button">Search</button>
+        <p className="header__explore">
+          Explore the world of (my) books with a click
+        </p>
+
+        <div className="header__search">
+          <input
+            className="header__search header__search--input"
+            type="text"
+            placeholder="Search..."
+          />
+          <button className="header__button">Search</button>
+        </div>
       </div>
     </header>
   );
