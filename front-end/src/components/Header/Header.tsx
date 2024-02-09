bd5c8942b94c29ce6b31706b39278f7624b004a6
Jowita Gogol <jmg88@bath.ac.uk>
2024-02-09 16:04:33 +0000

    Added Header component and applied initial styles

---
+++ front-end/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <h1 className="header__greeting">Hey there, bookaholic!</h1>
+        <p className="header__explore">Explore the world of (my) books with a click</p>
+      </div>
+      <div className="header__search">
+        <input className="header__search header__search--input" type="text" placeholder="Search..." />
+        <button className="header__button">Search</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
