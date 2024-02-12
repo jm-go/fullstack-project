e4874a7b85f9466123396e6959453fca39703fff
Jowita Gogol <jmg88@bath.ac.uk>
2024-02-12 23:14:39 +0000

    Updated data

--- front-end/src/components/data/books.ts
+++ front-end/src/components/data/books.ts
@@ -3,7 +3,7 @@ export const mockBooks = [
     cover: "src/assets/images/salem.jpg",
     title: "Salem's Lot",
     author: "Stephen King",
-    description: "A writer discovers his small town is overrun with vampires.",
+    description: "Salem's Lot is the story of a small American town being overtaken by vampires, and a brave band of people who come together to fight an ancient evil.",
     genre: "Horror",
     longDescription:
       "Author Ben Mears returns to ‘Salem's Lot to write a book about a house that has haunted him since childhood only to find his isolated hometown infested with vampires. While the vampires claim more victims, Mears convinces a small group of believers to combat the undead.",
@@ -14,7 +14,7 @@ export const mockBooks = [
     title: "The Taker",
     author: "Alma Katsu",
     description:
-      "True love can last an eternity... but immortality comes at a price.",
+      "True love can last an eternity… but immortality comes at a price. On the midnight shift, Dr. Findley is expecting another quiet evening. But when Lanore McIlvrae enters his ER, she changes his life forever.",
     genre: "Paranormal Romance",
     longDescription:
       "On the midnight shift at a hospital in rural St. Andrew, Maine, Dr. Luke Findley is expecting a quiet evening--until a mysterious woman, Lanore McIlvrae, arrives in his ER, escorted by police. Lanore is a murder suspect, and Luke is inexplicably drawn to her. As Lanny tells him her story, an impassioned account of love and betrayal that transcends time and mortality, she changes his life forever. At the turn of the nineteenth century, Lanny was consumed as a child by her love for the son of St. Andrew’s founder, and she will do anything to be with him forever, but the price she pays is steep--an immortal bond that chains her to a terrible fate.",
@@ -25,7 +25,7 @@ export const mockBooks = [
     title: "The Passage",
     author: "Justin Cronin",
     description:
-      "It happened fast. Thirty-two minutes for one world to die, another to be born.",
+      "Thirty-two minutes for one world to die, another to be born. An epic chronicle of human endurance in the face of unprecedented catastrophe and unimaginable danger.",
     genre: "Post Apocalyptic",
     longDescription:
       "First, the unthinkable: a security breach at a secret U.S. government facility unleashes the monstrous product of a chilling military experiment. Then, the unspeakable: a night of chaos and carnage gives way to sunrise on a nation, and ultimately a world, forever altered. All that remains for the stunned survivors is the long fight ahead and a future ruled by fear—of darkness, of death, of a fate far worse. As civilization swiftly crumbles into a primal landscape of predators and prey, two people flee in search of sanctuary. FBI agent Brad Wolgast is a good man haunted by what he's done in the line of duty. Six-year-old orphan Amy Harper Bellafonte is a refugee from the doomed scientific project that has triggered apocalypse. Wolgast is determined to protect her from the horror set loose by her captors, but for Amy, escaping the bloody fallout is only the beginning of a much longer odyssey—spanning miles and decades—toward the time and place where she must finish what should never have begun.",
@@ -46,7 +46,7 @@ export const mockBooks = [
     cover: "src/assets/images/dracula.jpg",
     title: "Dracula",
     author: "Bram Stoker",
-    description: "The iconic 1897 tale of a vampire from Transylvania.",
+    description: "The great masterpiece of the horror genre, brilliantly evoking a nightmare world of vampires and vampire hunters and also illuminating the dark corners of Victorian desire.",
     genre: "Horror",
     longDescription:
       "When Jonathan Harker visits Transylvania to help Count Dracula with the purchase of a London house, he makes a series of horrific discoveries about his client. Soon afterwards, various bizarre incidents unfold in England: an apparently unmanned ship is wrecked off the coast of Whitby; a young woman discovers strange puncture marks on her neck; and the inmate of a lunatic asylum raves about the 'Master' and his imminent arrival. In Dracula, Bram Stoker created one of the great masterpieces of the horror genre, brilliantly evoking a nightmare world of vampires and vampire hunters and also illuminating the dark corners of Victorian sexuality and desire.",
@@ -94,7 +94,7 @@ export const mockBooks = [
     genre: "Horror",
     longDescription:
       "Tamsen Donner must be a witch. That is the only way to explain the series of misfortunes that have plagued the wagon train known as the Donner Party. Depleted rations, bitter quarrels, and the mysterious death of a little boy have driven the pioneers to the brink of madness. They cannot escape the feeling that someone--or something--is stalking them. Whether it was a curse from the beautiful Tamsen, the choice to follow a disastrous experimental route West, or just plain bad luck--the 90 men, women, and children of the Donner Party are at the brink of one of the deadliest and most disastrous western adventures in American history. While the ill-fated group struggles to survive in the treacherous mountain conditions--searing heat that turns the sand into bubbling stew; snows that freeze the oxen where they stand--evil begins to grow around them, and within them. As members of the party begin to disappear, they must ask themselves 'What if there is something waiting in the mountains? Something disturbing and diseased...and very hungry?'",
-    favourite: true,
+    favourite: false,
   },
   {
     cover: "src/assets/images/garden.jpg",
@@ -106,5 +106,16 @@ export const mockBooks = [
     longDescription:
       "When a human skull is dug up in a garden near Boston, Dr Maura Isles is called in to investigate. She quickly discovers that the skeleton - that of a young woman - has been buried for over a hundred years. But who was the woman? And how did she die? It is the 1930s, and an impoverished medical student, Norris Marshall, is forced to procure corpses in order to further his studies in human anatomy. It's a gruesome livelihood that will bring him into contact with a terrifying serial killer who slips from ballrooms to graveyards and into autopsy suites. And who is far, far closer than Norris could ever imagine...",
     favourite: true,
+  },
+  {
+    cover: "src/assets/images/fevre.jpg",
+    title: "Fevre Dream",
+    author: "George R.R. Martin",
+    description:
+      "The spellbinding tale of a vampire’s quest to unite his race with humanity, of a garrulous riverman’s dream of immortality, and of the undying legends of the steamboat era and a majestic, ancient river.",
+    genre: "Fantasy",
+    longDescription:
+      "When struggling riverboat captain Abner Marsh receives an offer of partnership from a wealthy aristocrat, he suspects something’s amiss. But when he meets the hauntingly pale, steely-eyed Joshua York, he is certain. For York doesn’t care that the icy winter of 1857 has wiped out all but one of Marsh’s dilapidated fleet. Nor does he care that he won’t earn back his investment in a decade. York has his own reasons for wanting to traverse the powerful Mississippi. And they are to be none of Marsh’s concern—no matter how bizarre, arbitrary, or capricious his actions may prove. Marsh meant to turn down York’s offer. It was too full of secrets that spelled danger. But the promise of both gold and a grand new boat that could make history crushed his resolve—coupled with the terrible force of York’s mesmerizing gaze. Not until the maiden voyage of his new sidewheeler Fevre Dream would Marsh realize he had joined a mission both more sinister, and perhaps more noble, than his most fantastic nightmare...and mankind’s most impossible dream. Here is the spellbinding tale of a vampire’s quest to unite his race with humanity, of a garrulous riverman’s dream of immortality, and of the undying legends of the steamboat era and a majestic, ancient river.",
+    favourite: false,
   },
 ];
