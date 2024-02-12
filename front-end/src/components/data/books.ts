9bc0ddefba7478affe68b81e69044fd355aac726
Jowita Gogol <jmg88@bath.ac.uk>
2024-02-12 20:32:59 +0000

    Added temporary database

--- front-end/src/components/data/books.ts
+++ front-end/src/components/data/books.ts
@@ -1,67 +1,110 @@
-export const mockBook = {
-  cover: "favourites/src/assets/images/cover.jpg",
-  title: "The Old Man and the X",
-  author: "Ernest Hemingway",
-  description:
-    "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
-};
-
 export const mockBooks = [
   {
-    cover: "src/assets/images/cover.jpg",
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
-    description:
-      // "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
-      `The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.`,
+    cover: "src/assets/images/salem.jpg",
+    title: "Salem's Lot",
+    author: "Stephen King",
+    description: "A writer discovers his small town is overrun with vampires.",
+    genre: "Horror",
+    longDescription:
+      "Author Ben Mears returns to ‘Salem's Lot to write a book about a house that has haunted him since childhood only to find his isolated hometown infested with vampires. While the vampires claim more victims, Mears convinces a small group of believers to combat the undead.",
+    favourite: false,
   },
   {
-    cover: "src/assets/images/cover.jpg",
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
+    cover: "src/assets/images/taker.jpg",
+    title: "The Taker",
+    author: "Alma Katsu",
     description:
-      "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
+      "True love can last an eternity... but immortality comes at a price.",
+    genre: "Paranormal Romance",
+    longDescription:
+      "On the midnight shift at a hospital in rural St. Andrew, Maine, Dr. Luke Findley is expecting a quiet evening--until a mysterious woman, Lanore McIlvrae, arrives in his ER, escorted by police. Lanore is a murder suspect, and Luke is inexplicably drawn to her. As Lanny tells him her story, an impassioned account of love and betrayal that transcends time and mortality, she changes his life forever. At the turn of the nineteenth century, Lanny was consumed as a child by her love for the son of St. Andrew’s founder, and she will do anything to be with him forever, but the price she pays is steep--an immortal bond that chains her to a terrible fate.",
+    favourite: true,
   },
   {
-    cover: "src/assets/images/cover.jpg",
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
+    cover: "src/assets/images/passage.jpg",
+    title: "The Passage",
+    author: "Justin Cronin",
     description:
-      "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
+      "It happened fast. Thirty-two minutes for one world to die, another to be born.",
+    genre: "Post Apocalyptic",
+    longDescription:
+      "First, the unthinkable: a security breach at a secret U.S. government facility unleashes the monstrous product of a chilling military experiment. Then, the unspeakable: a night of chaos and carnage gives way to sunrise on a nation, and ultimately a world, forever altered. All that remains for the stunned survivors is the long fight ahead and a future ruled by fear—of darkness, of death, of a fate far worse. As civilization swiftly crumbles into a primal landscape of predators and prey, two people flee in search of sanctuary. FBI agent Brad Wolgast is a good man haunted by what he's done in the line of duty. Six-year-old orphan Amy Harper Bellafonte is a refugee from the doomed scientific project that has triggered apocalypse. Wolgast is determined to protect her from the horror set loose by her captors, but for Amy, escaping the bloody fallout is only the beginning of a much longer odyssey—spanning miles and decades—toward the time and place where she must finish what should never have begun.",
+    favourite: true,
   },
   {
-    cover: "src/assets/images/cover.jpg",
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
+    cover: "src/assets/images/twilight.jpg",
+    title: "Twilight",
+    author: "Stephenie Meyer",
     description:
-      "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
+      "A young adult adventure-romance about a teenage girl who falls in love with a vampire.",
+    genre: "Supernatural",
+    longDescription:
+      "When 17 year old Isabella Swan moves to Forks, Washington to live with her father she expects that her new life will be as dull as the town. But in spite of her awkward manner and low expectations, she finds that her new classmates are drawn to this pale, dark-haired new girl in town. But not, it seems, the Cullen family. These five adopted brothers and sisters obviously prefer their own company and will make no exception for Bella. Bella is convinced that Edward Cullen in particular hates her, but she feels a strange attraction to him, although his hostility makes her feel almost physically ill. He seems determined to push her away - until, that is, he saves her life from an out of control car. Bella will soon discover that there is a very good reason for Edward's coldness. He, and his family, are vampires - and he knows how dangerous it is for others to get too close.",
+    favourite: false,
   },
   {
-    cover: "src/assets/images/cover.jpg",
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
-    description:
-      "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
+    cover: "src/assets/images/dracula.jpg",
+    title: "Dracula",
+    author: "Bram Stoker",
+    description: "The iconic 1897 tale of a vampire from Transylvania.",
+    genre: "Horror",
+    longDescription:
+      "When Jonathan Harker visits Transylvania to help Count Dracula with the purchase of a London house, he makes a series of horrific discoveries about his client. Soon afterwards, various bizarre incidents unfold in England: an apparently unmanned ship is wrecked off the coast of Whitby; a young woman discovers strange puncture marks on her neck; and the inmate of a lunatic asylum raves about the 'Master' and his imminent arrival. In Dracula, Bram Stoker created one of the great masterpieces of the horror genre, brilliantly evoking a nightmare world of vampires and vampire hunters and also illuminating the dark corners of Victorian sexuality and desire.",
+    favourite: false,
   },
   {
-    cover: "src/assets/images/cover.jpg",
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
+    cover: "src/assets/images/empire.jpg",
+    title: "Empire of The Vampire",
+    author: "Jay Kristoff",
     description:
-      "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
+      "A story of a warrior who fights the darkness in a world where the sun has been dimmed and Vampires walk the earth, preying endlessly on humanity.",
+    genre: "Dark Fantasy",
+    longDescription:
+      "It has been twenty-seven long years since the last sunrise. Ever since, vampires have waged war against humanity building their eternal empire even as they tear down our own. Gabriel de León, half man, half monster, and last remaining silversaint – a sworn brother of the holy Silver Order dedicated to defending the realm from the creatures of the night – is all that stands between the world and its end. Imprisoned by the very monsters he has vowed to destroy Gabriel is forced to tell his story – a story of legendary battles and forbidden love, of faith lost and friendships won, of the War of the Blood and the Forever King and the quest for humanity’s last remaining hope: The Holy Grail.",
+    favourite: true,
   },
   {
-    cover: "src/assets/images/cover.jpg",
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
+    cover: "src/assets/images/mages.jpg",
+    title: "Magicians' Guild",
+    author: "Trudi Canavan",
     description:
-      "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
+      "Sonea, a young girl from the slums, discovers her latent magical potential and finds herself on the run from the powerful Magicians' Guild.",
+    genre: "Fantasy",
+    longDescription:
+      "This year, like every other, the magicians of Imardin gather to purge the city of undesirables. Cloaked in the protection of their sorcery, they move with no fear of the vagrants and miscreants who despise them and their work-—until one enraged girl, barely more than a child, hurls a stone at the hated invaders...and effortlessly penetrates their magical shield. What the Magicians' Guild has long dreaded has finally come to pass. There is someone outside their ranks who possesses a raw power beyond imagining, an untrained mage who must be found and schooled before she destroys herself and her city with a force she cannot yet control.",
+    favourite: false,
   },
   {
-    cover: "src/assets/images/cover.jpg",
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
+    cover: "src/assets/images/witcher.jpg",
+    title: "The Witcher: Blood of Elves",
+    author: "Andrzej Sapkowski",
     description:
-      "The story of an epic struggle between an old, experienced fisherman and the greatest catch of his life.",
+      "Into this tumultuous time is born a child for whom the the world has been waiting, one with the power to change the world - for good, or for evil ...",
+    genre: "Fantasy",
+    longDescription:
+      "For more than a hundred years, humans, dwarves, gnomes and elves lived together in relative peace. But times have changed, the uneasy peace is over and now the races are fighting once again - killing their own kind and each other. Into this tumultuous time is born a child of prophecy, Ciri, surviving heiress of a bloody revolution, whose strange abilities can change the world - for good, or for evil... As the threat of war hangs over the land, Geralt the Witcher must protect Ciri from those who are hunting the child for her destructive power. But this time, Geralt may have met his match.",
+    favourite: false,
+  },
+  {
+    cover: "src/assets/images/hunger.jpg",
+    title: "The Hunger",
+    author: "Alma Katsu",
+    description:
+      "A bone-chilling mix of historical record and visceral horror pits a group of increasingly desperate pioneers against the unknown dangers of the Old West.",
+    genre: "Horror",
+    longDescription:
+      "Tamsen Donner must be a witch. That is the only way to explain the series of misfortunes that have plagued the wagon train known as the Donner Party. Depleted rations, bitter quarrels, and the mysterious death of a little boy have driven the pioneers to the brink of madness. They cannot escape the feeling that someone--or something--is stalking them. Whether it was a curse from the beautiful Tamsen, the choice to follow a disastrous experimental route West, or just plain bad luck--the 90 men, women, and children of the Donner Party are at the brink of one of the deadliest and most disastrous western adventures in American history. While the ill-fated group struggles to survive in the treacherous mountain conditions--searing heat that turns the sand into bubbling stew; snows that freeze the oxen where they stand--evil begins to grow around them, and within them. As members of the party begin to disappear, they must ask themselves 'What if there is something waiting in the mountains? Something disturbing and diseased...and very hungry?'",
+    favourite: true,
+  },
+  {
+    cover: "src/assets/images/garden.jpg",
+    title: "The Bone Garden",
+    author: "Tess Gerritsen",
+    description:
+      "Unknown bones, untold secrets, and unsolved crimes from the  distant past cast ominous shadows on the present.",
+    genre: "Thriller",
+    longDescription:
+      "When a human skull is dug up in a garden near Boston, Dr Maura Isles is called in to investigate. She quickly discovers that the skeleton - that of a young woman - has been buried for over a hundred years. But who was the woman? And how did she die? It is the 1930s, and an impoverished medical student, Norris Marshall, is forced to procure corpses in order to further his studies in human anatomy. It's a gruesome livelihood that will bring him into contact with a terrifying serial killer who slips from ballrooms to graveyards and into autopsy suites. And who is far, far closer than Norris could ever imagine...",
+    favourite: true,
   },
 ];
--- front-end/src/types/book.ts
+++ front-end/src/types/book.ts
@@ -3,6 +3,9 @@ type Book = {
   title: string;
   author: string;
   description: string;
+  genre: string;
+  longDescription: string;
+  favourite: boolean;
 };
 
 export default Book;
