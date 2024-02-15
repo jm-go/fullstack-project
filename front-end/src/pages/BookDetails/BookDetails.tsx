e28ecd7e9142914da1f8507333eaa431e0e4078e
Jowita Gogol <jmg88@bath.ac.uk>
2024-02-15 17:33:07 +0000

    Updated styles for error message

--- front-end/src/pages/BookDetails/BookDetails.tsx
+++ front-end/src/pages/BookDetails/BookDetails.tsx
@@ -11,41 +11,38 @@ type BookDetailsProps = {
 
 const BookDetails = ({ books }: BookDetailsProps) => {
   const { id } = useParams<{ id: string }>();
-
   const selectedBook = books.find((book) => book.id === Number(id));
-
-  if (!selectedBook) return <div className="book-details__message">No such book found.</div>;
-
-  const {
-    title,
-    cover,
-    descriptionLong,
-    authorId: { firstName, lastName } = {},
-    genre,
-    isFavourite,
-  } = selectedBook;
 
   return (
     <Layout>
       <div className="book-details">
         <Sidebar />
         <div className="book-details__main">
-          <div className="book-details__cover">
-            <img
-              className="book-details__image"
-              src={cover || BookCover}
-              alt={title}
-            />
-          </div>
-          <div className="book-details__info">
-            <h2 className="book-details__title">{title}</h2>
-            <p className="book-details__author">{`${firstName} ${lastName}`}</p>
-            <p className="book-details__genre">{genre}</p>
-            <p className="book-details__favourite">
-              Favourite: {isFavourite === "true" ? " Yes" : " No"}
-            </p>
-            <p className="book-details__description">{descriptionLong}</p>
-          </div>
+          {selectedBook ? (
+            <>
+              <div className="book-details__cover">
+                <img
+                  className="book-details__image"
+                  src={selectedBook.cover || BookCover}
+                  alt={selectedBook.title}
+                />
+              </div>
+              <div className="book-details__info">
+                <h2 className="book-details__title">{selectedBook.title}</h2>
+                <p className="book-details__author">{`${selectedBook.authorId.firstName} ${selectedBook.authorId.lastName}`}</p>
+                <p className="book-details__genre">{selectedBook.genre}</p>
+                <p className="book-details__favourite">
+                  Favourite:{" "}
+                  {selectedBook.isFavourite === "true" ? " Yes" : " No"}
+                </p>
+                <p className="book-details__description">
+                  {selectedBook.descriptionLong}
+                </p>
+              </div>
+            </>
+          ) : (
+            <div className="book-details__message">Oops! It looks like this book is not in my collection.</div>
+          )}
         </div>
       </div>
     </Layout>
